7a2530f1780cb1d886e41a6deaf19d2c91c01e8d
Bezabee <bezaberhanu92@gmail.com>
2026-05-08 15:59:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 home_page = st.Page("Home.py", title="Home", default=True)
-calc_page = st.Page("pages/gpa_calculator.py", title="GPA Calculator", icon="🧮")
-trans_page = st.Page("pages/transcript_and_cgpa.py", title="Transcript", icon="📜")
-admin_page = st.Page("admin_portal.py", title="Admin Panel", icon="🛡️")
+calc_page = st.Page("pages/gpa_calculator.py", title="GPA Calculator")
+trans_page = st.Page("pages/transcript_and_cgpa.py", title="Transcript")
+admin_page = st.Page("admin_portal.py", title="Admin Panel")
 
 my_pages = [home_page, calc_page, trans_page]
 
